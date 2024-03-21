780754d19b7555332e7bef9c1c4f41cc87ec9414
ADRIELE1823 <136723178+ADRIELE1823@users.noreply.github.com>
2024-03-21 08:46:34 -0300

    Add files via upload

---
+++ src/app/componentes/logar/logar/logar.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logar',
+  templateUrl: './logar.component.html',
+  styleUrl: './logar.component.css'
+})
+export class LogarComponent {
+
+}
