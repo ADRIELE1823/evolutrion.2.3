90b1d10a3226f1de22794494bc0a9f75026cbc86
Douglas <douglasevertonev@gmail.com>
2025-01-16 18:11:32 -0300

    fix: path painel component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PrincipalComponent } from './componentes/principal/principal.component';
 import { WelcomeComponent } from './componentes/welcome/welcome.component';
-import { PainelComponent } from './componentes/painel/painel.component';
+import { PainelComponent } from './componentes/painel/painel/painel/painel.component';
 import { CabecalhoComponent } from './componentes/cabecalho/cabecalho.component';
 import { LogarComponent } from './componentes/logar/logar.component';
 import { AboutMeComponent } from './componentes/about-me/about-me.component';
